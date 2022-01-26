48c09259a4c9efc816996ea04b55ef7a83351fbf
AntonioGeorgiev <95080861+AntonioGeorgiev@users.noreply.github.com>
2022-01-26 11:54:33 +0200

    Add files via upload

---
+++ CarShop.Test/CarTests.cs
@@ -0,0 +1,249 @@
+﻿using AutoMapper;
+using ECarShop.Controllers;
+using ECarShop.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Xunit;
+
+namespace ECarShop.Test
+{
+    public class CarTests
+    {
+            private readonly IMapper _mapper;
+            private readonly Mock<ICarRepository> _carRepository;
+            private readonly ICarService _carService;
+            private readonly CarController _carController;
+
+        new Car()
+         {
+                Id = 1,
+                Brand = "Opel",
+                Model = "Vectra",
+                 Price = 5000,
+                
+            },
+            new Car()
+           {
+                Id = 2,
+                Brand = "BMW",
+                Model = "E60",
+                Price = 10000,
+            },
+            new Car()
+             {
+                Id = 3,
+                Brand = "Mercedes Benz",
+                Model = "S500",
+               Price = 15000,
+            },    
+
+            };
+
+
+            public CarTests()
+            {
+                var mockMapper = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new AutoMapping());
+                });
+
+                _mapper = mockMapper.CreateMapper();
+
+                _carRepository = new Mock<ICarRepository>();
+
+                var logger = new Mock<ILogger>();
+
+                _carService = new CarService(_carRepository.Object);
+
+                _carController = new CarController(_carService, _mapper);
+            }
+
+            [Fact]
+            public void Car_GetAll_Count_Check()
+            {
+                //setup
+                var expectedCount = 3;
+
+                var mockedService = new Mock<ICarService>();
+
+                mockedService.Setup(x => x.GetAll()).Returns(Cars);
+
+                //inject
+                var controller = new CarController(mockedService.Object, _mapper);
+
+                //Act
+                var result = controller.GetAll();
+
+                //Assert
+                var okObjectResult = result as OkObjectResult;
+                Assert.NotNull(okObjectResult);
+                Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+                var cars = okObjectResult.Value as IEnumerable<Car>;
+                Assert.NotNull(cars);
+               Assert.Equal(expectedCount, Cars.Count());
+            }
+
+            [Fact]
+           public void Car_GetById_Amount_Check()
+            {
+                //setup
+                var carId = 2;
+                var expectedPrice = 10000;
+
+                _carRepository.Setup(x => x.GetById(carId))
+                    .Returns(Cars.FirstOrDefault(t => t.Id == carId));
+
+                //Act
+                var result = _carController.GetById(carId);
+
+                //Assert
+                var okObjectResult = result as OkObjectResult;
+                Assert.NotNull(okObjectResult);
+                Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+                var response = okObjectResult.Value as CarResponse;
+                var car = _mapper.Map<Car>(response);
+
+                Assert.NotNull(car);
+                Assert.Equal(expectedAmount, car.Price);
+
+            }
+
+            [Fact]
+            public void Car_GetById_NotFound()
+            {
+                //setup
+                var carId = 4;
+
+                _carRepository.Setup(x => x.GetById(carId))
+                    .Returns(Car.FirstOrDefault(t => t.Id == carId));
+
+                //Act
+                var result = _carController.GetById(carId);
+
+                //Assert
+                var notFoundObjectResult = result as NotFoundObjectResult;
+                Assert.NotNull(notFoundObjectResult);
+                Assert.Equal(notFoundObjectResult.StatusCode, (int)HttpStatusCode.NotFound);
+
+                var response = (int)notFoundObjectResult.Value;
+                Assert.Equal(carId, response);
+            }
+
+            [Fact]
+            public void Car_Update_CarPrice()
+            {
+                var carId = 2;
+                var expectedPrice =  10000;
+
+                var car = Cars.FirstOrDefault(x => x.Id == carId);
+                car.Amount = expectedPrice;
+
+                _carRepository.Setup(x => x.GetById(carId))
+                    .Returns(Cars.FirstOrDefault(t => t.Id == carId));
+                _carRepository.Setup(x => x.Update(car))
+                    .Returns(car);
+
+                //Act
+                var carRequest = _mapper.Map<CarRequest>(car);
+                var result = _carController.Update(car);
+
+                //Assert
+                var okObjectResult = result as OkObjectResult;
+                Assert.NotNull(okObjectResult);
+                Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+                var val = okObjectResult.Value as Car
+                Assert.NotNull(val);
+                Assert.Equal(expectedAmount, val.Price);
+
+            }
+
+            [Fact]
+            public void Car_Delete_ExistingCar()
+            {
+                //Setup
+                var carId=2;
+
+                var car = Cars.FirstOrDefault(x => x.Id == carId);
+
+                _carRepository.Setup(x => x.Delete(carId)).Callback(() => Cars.Remove(car)).Returns(car);
+
+                //Act
+                var result = _carController.Delete(carId);
+
+                //Assert
+                var okObjectResult = result as OkObjectResult;
+                Assert.NotNull(okObjectResult);
+                Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+                var val = okObjectResult.Value as Car;
+                Assert.NotNull(val);
+                Assert.Equal(2, Cars.Count);
+                Assert.Null(Cars.FirstOrDefault(x => x.Id == carId));
+            }
+
+            [Fact]
+            public void Car_Delete_NotExisting_Tag()
+            {
+                //Setup
+                var carId= 6;
+
+                var car = Cars.FirstOrDefault(x => x.Id == carId);
+
+                _carRepository.Setup(x => x.Delete(carId)).Callback(() => Cars.Remove(car).Returns(car);
+
+                //Act
+                var result = _carController.Delete(carId);
+
+                //Assert
+                var notFoundObjectResult = result as NotFoundObjectResult;
+                Assert.NotNull(notFoundObjectResult);
+                Assert.Equal(notFoundObjectResult.StatusCode, (int)HttpStatusCode.NotFound);
+
+                var response = (int)notFoundObjectResult.Value;
+                Assert.Equal(carId, response);
+            }
+
+            [Fact]
+            public void Car_CreateTag()
+            {
+                //setup
+                var car = new Car()
+                {
+
+                     Id = 4,
+                Brand = "Car4",
+                Model = "Model4",
+               Price = 10000,
+
+               };
+
+                _carRepository.Setup(x => x.GetAll()).Returns(Cars);
+
+                _CarRepository.Setup(x => x.Create(It.IsAny<Car>())).Callback(() =>
+                {
+                    Cars.Add(car);
+                }).Returns(car);
+
+                //Act
+                var result = _carController.CreateCar(_mapper.Map<CarRequest>(car));
+
+                //Assert
+                var okObjectResult = result as OkObjectResult;
+
+                Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+                Assert.NotNull(Cars.FirstOrDefault(x => x.Id == car.Id));
+                Assert.Equal(4, Cars.Count);
+
+            }
+
+        }
+    }
+}
---
+++ CarShop.Test/DealerTests.cs
@@ -0,0 +1,244 @@
+﻿using ECarShop.Models.DTO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using AutoMapper;
+using ECarShop.BL.Interfaces;
+using ECarShop.BL.Services;
+using ECarShop.Controllers;
+using ECarShop.DL.Interfaces;
+using ECarShop.Extensions;
+using ECarShop.Models.Requests;
+using ECarShop.Models.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Serilog;
+using Xunit;
+using ECarShop.Host.Controllers;
+
+namespace ECarShop.Test
+{
+    public class DealerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IDealerRepository> _dealerRepository;
+        private readonly IDealerService _dealerService;
+        private readonly DealerController _dealerController;
+
+        private IList<Dealer> Dealers = new List<Dealer>()
+        {
+ new Dealer()
+            {
+                Id = 1,
+                Name = "Dealer1",
+                PhoneNumber = 696969
+            },
+            new Dealer()
+            {
+                Id = 2,
+                Name = "Dealer2",
+                PhoneNumber = 123456
+            },
+            new Dealer()
+            {
+                Id = 3,
+                Name = "Dealer3",
+                PhoneNumber = 875600
+            } };
+
+        public DealerTests()
+        {
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapping());
+            });
+            _mapper = mockMapper.CreateMapper();
+
+            _dealerRepository = new Mock<IDealerRepository>();
+
+            var logger = new Mock<ILogger>();
+
+            _dealerService = new DealerService(_dealerRepository.Object);
+            _dealerController = new DealerController(_dealerService, _mapper);
+
+        }
+        [Fact]
+        public void Dealer_GetAll_Count_Check()
+        {
+            //setup
+            var expectedCount = 2;
+
+            var mockedService = new Mock<IService>();
+
+            mockedService.Setup(x => x.GetAll()).Returns(Dealers);
+
+            //inject
+            var controller = new DealerController(mockedService.Object, _mapper);
+
+            //Act
+            var result = controller.GetAll();
+
+            //Assert
+            var okObjectResult = result as OkObjectResult;
+            Assert.NotNull(okObjectResult);
+            Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+            var dealers= okObjectResult.Value as IEnumerable<Dealer>;
+            Assert.NotNull(dealers);
+            Assert.Equal(expectedCount, dealers.Count());
+        }
+
+        [Fact]
+        public void Dealer_GetById_NameCheck()
+        {
+            //setup
+            var DealerId = 2;
+            var expectedName = "Dealer2";
+
+            _dealerRepository.Setup(x => x.GetById(dealerId))
+                .Returns(Dealers.FirstOrDefault(e => e.Id == dealerId));
+
+            //Act
+            var result = _dealerController.GetById(dealerId);
+
+            //Assert
+            var okObjectResult = result as OkObjectResult;
+            Assert.NotNull(okObjectResult);
+            Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+            var response = okObjectResult.Value as DealerResponse;
+            var dealer = _mapper.Map<Dealer>(response);
+
+            Assert.NotNull(dealer);
+            Assert.Equal(expectedName, dealer.Name);
+        }
+
+        [Fact]
+        public void Dealer_GetById_NotFound()
+        {
+            //setup
+            var dealerId = 3;
+
+            _dealerRepository.Setup(x => x.GetById(dealerId))
+                .Returns(Dealers.FirstOrDefault(t => t.Id == dealerId));
+
+            //Act
+            var result = _dealerController.GetById(dealerId);
+
+            //Assert
+            var notFoundObjectResult = result as NotFoundObjectResult;
+            Assert.NotNull(notFoundObjectResult);
+            Assert.Equal(notFoundObjectResult.StatusCode, (int)HttpStatusCode.NotFound);
+
+            var response = (int)notFoundObjectResult.Value;
+            Assert.Equal(dealerId, response);
+        }
+
+        [Fact]
+        public void Dealer_Update_DealerName()
+        {
+            var DealerId = 1;
+            var expectedName = "Updated Dealer Name";
+
+            var dealer = Dealers.FirstOrDefault(x => x.Id == dealerId);
+            dealer.Name = expectedName;
+
+            _dealerRepository.Setup(x => x.GetById(dealerId))
+                .Returns(Dealers.FirstOrDefault(t => t.Id == dealerId));
+            _dealerRepository.Setup(x => x.Update(dealer))
+                .Returns(dealer);
+
+            //Act
+            var dealerUpdateRequest = _mapper.Map<DealerUpdateRequest>(dealer);
+            var result = _dealerController.Update(dealerUpdateRequest);
+
+            //Assert
+            var okObjectResult = result as OkObjectResult;
+            Assert.NotNull(okObjectResult);
+            Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+            var val = okObjectResult.Value as Dealer;
+            Assert.NotNull(val);
+            Assert.Equal(expectedName, val.Name);
+
+        }
+
+        [Fact]
+        public void Dealer_Delete_ExistingDealer()
+        {
+            //Setup
+            var dealerId = 1;
+
+            var dealer = Dealers.FirstOrDefault(x => x.Id == dealerId);
+
+            _dealerRepository.Setup(x => x.Delete(dealerId)).Callback(() => Dealers.Remove(dealer)).Returns(dealer);
+
+            //Act
+            var result = _dealerController.Delete(dealerId);
+
+            //Assert
+            var okObjectResult = result as OkObjectResult;
+            Assert.NotNull(okObjectResult);
+            Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+
+            var val = okObjectResult.Value as Dealer;
+            Assert.NotNull(val);
+            Assert.Equal(1, Dealers.Count);
+            Assert.Null(Dealers.FirstOrDefault(x => x.Id == dealerId));
+        }
+
+        [Fact]
+        public void Dealer_Delete_NotExisting_Dealer()
+        {
+            //Setup
+            var dealerId = 3;
+
+            var dealer = Dealers.FirstOrDefault(x => x.Id == dealerId);
+
+            _dealerRepository.Setup(x => x.Delete(dealerId)).Callback(() => Dealers.Remove(dealer)).Returns(dealer);
+
+            //Act
+            var result = _dealerController.Delete(dealerId);
+
+            //Assert
+            var notFoundObjectResult = result as NotFoundObjectResult;
+            Assert.NotNull(notFoundObjectResult);
+            Assert.Equal(notFoundObjectResult.StatusCode, (int)HttpStatusCode.NotFound);
+
+            var response = (int)notFoundObjectResult.Value;
+            Assert.Equal(dealerId, response);
+        }
+
+        [Fact]
+        public void Dealer_CreateDealer()
+        {
+            //setup
+            var dealer = new Dealer()
+            {
+                Id = 4,
+                Name = "Dealer4",
+                PhoneNumber = 000000,
+            };
+
+        _dealerRepository.Setup(x => x.GetAll()).Returns(Dealers);
+
+        _dealerRepository.Setup(x => x.Create(It.IsAny<Dealer>())).Callback(() =>
+            {
+                Dealers.Add(dealer);
+            }).Returns(dealer);
+
+            //Act
+            var result = _dealerController.CreateDealer(_mapper.Map<DealerRequest>(dealer));
+
+            //Assert
+            var okObjectResult = result as OkObjectResult;
+
+            Assert.Equal(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+            Assert.NotNull(Dealers.FirstOrDefault(x => x.Id == dealer.Id));
+            Assert.Equal(3, Dealers.Count);
+
+        }
+
+
+    }
+}
